2342587ee366e9232a5838136f95813f36de0a56
slavan72@mail.ru <slavan72@mail.ru>
2021-07-21 07:29:01 +0300

    Created main project file.

---
+++ Auto_Compensation_Interference/Auto_Compensation_Interference.cpp
@@ -0,0 +1,15 @@
+// Auto_Compensation_Interference.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+
+	printf("Hello world!");
+	_getch();
+
+    return 0;
+}
+
