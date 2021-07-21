f3fce14835ba087159188d65e7f66f8a864ece4b
slavan72@mail.ru <slavan72@mail.ru>
2021-07-21 09:23:18 +0300

    Add support for deprecated functions(Properties >>C/C++ >>Preporocessor >> Preprocessor Определения >> _CRT_SECURE_NO_WARNINGS).

--- Auto_Compensation_Interference/Auto_Compensation_Interference.cpp
+++ Auto_Compensation_Interference/Auto_Compensation_Interference.cpp
@@ -2,14 +2,32 @@
 //
 
 #include "stdafx.h"
+#include "Console_String_LIB.h"
 
 
 int main()
 {
+	double b = 0;//23.45;
+	//double b1 = 1.234;
+	//double b2 = 5.678;
+	//double b3 = 9.10;
+	long int ll = 0;
+	//char r1[] = "Hello";
 
-	printf("Hello world!");
+	printf("\nb = ");
+	scanf("%lf", &b);
+	printf("\nll = ");
+	scanf("%ld", &ll);
+	OU1(b, ll);
+	//OU2(ll);
+	//OU(b, ll);
+	// OF1(b, (int)ll, r1);
+	// OFN (b, b1,b2,b3, (int)ll, r1);
+	//OFP(b, ll,r1);
+
+
+	//printf("Hello world!");
 	_getch();
-
     return 0;
 }
 
